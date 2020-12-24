5753fba3ce1669fd893921795a4a4d5dfd341e85
Whu_hhx <1967943495@qq.com>
2020-12-24 08:58:34 +0800

    完成tomcat环境配置

--- src/main/java/com/favorites/FavoritesApplication.java
+++ src/main/java/com/favorites/FavoritesApplication.java
@@ -2,11 +2,18 @@ package com.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class FavoritesApplication {
-
+@EnableScheduling
+public class FavoritesApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(FavoritesApplication.class, args);
+        SpringApplication.run(FavoritesApplication.class,args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FavoritesApplication.class);
     }
 }
--- src/main/java/com/favorites/controller/UserController.java
+++ src/main/java/com/favorites/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.favorites.controller;
-import com.favorites.service.IUserService;
+import com.favorites.domain.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-   // @RequestMapping(value = "/regist", method = RequestMethod.POST)
-    //public
+    @RequestMapping(value = "/regist", method = RequestMethod.POST)
+    public String regist(@RequestBody(required = false) User user){
+        System.out.println(user.toString());
+        return "fail";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
